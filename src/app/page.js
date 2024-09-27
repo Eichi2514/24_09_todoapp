eaa142cb63740c41d46dd7c7a644078b4bfc934f
Eichi2514 <picon1317@gmail.com>
2024-09-27 16:20:28 +0900

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,170 @@
+"use client";
+
+import { useState } from "react";
+import produce from "immer";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const NewTodoForm = ({ todoStatus }) => {
+  const [newTodoTitle, setNewTodoTitle] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const NewTodoFormKeyDown = (e) => {
+    if (e.key === "Enter") {
+      addTodo();
+    }
+  };
+
+  const addTodo = () => {
+    if (newTodoTitle.trim().length === 0) return;
+
+    const title = newTodoTitle.trim();
+    todoStatus.addTodo(title);
+    setNewTodoTitle("");
+  };
+
+  return (
+    <>
+      <div className="flex items-center gap-x-3 m-3">
+        <input
+          className="input input-bordered"
+          type="text"
+          placeholder="새 일정"
+          value={newTodoTitle}
+          onChange={(e) => setNewTodoTitle(e.target.value)}
+          onKeyDown={NewTodoFormKeyDown}
+        />
+        <button className="btn btn-neutral" onClick={addTodo}>
+          추가
+        </button>
+      </div>
+    </>
+  );
+};
+
+const TodoListItem = ({ todo, todoStatus }) => {
+  const [editMode, setEditMode] = useState(false);
+  const [newTodoTitle, setNewTodoTitle] = useState(todo.title);
+
+  const removeTodo = () => {
+    todoStatus.removeTodo(todo.id);
+  };
+
+  const changeEditMode = () => {
+    setNewTodoTitle(todo.title);
+    setEditMode(!editMode);
+  };
+
+  const modifyTodo = () => {
+    if (newTodoTitle.trim().length !== 0) {
+      todoStatus.modifyTodo(todo.id, newTodoTitle);
+    }
+    changeEditMode();
+  };
+
+  return (
+    <>
+      <li className="flex items-center gap-x-3 ml-3 mb-3">
+        <span className="badge badge-primary badge-outline">{todo.id}</span>
+        {editMode ? (
+          <>
+            <input
+              type="text"
+              className="input input-bordered"
+              placeholder="수정 내용 써"
+              value={newTodoTitle}
+              onChange={(e) => setNewTodoTitle(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <button className="btn btn-warning" onClick={modifyTodo}>
+              저장
+            </button>
+            <button className="btn btn-error" onClick={changeEditMode}>
+              취소
+            </button>
+          </>
+        ) : (
+          <>
+            <span>{todo.title}</span>
+            <button className="btn btn-success">메모</button>
+            <button className="btn btn-warning" onClick={changeEditMode}>
+              수정
+            </button>
+            <button className="btn btn-error" onClick={removeTodo}>
+              삭제
+            </button>
+          </>
+        )}
+      </li>
+    </>
+  );
+};
+
+const TodoList = ({ todoStatus }) => {
+  return (
+    <>
+      {todoStatus.todos.length === 0 ? (
+        <h4 className="m-3">오늘 일정 없음</h4>
+      ) : (
+        <>
+          <h4 className="m-3">일정 목록</h4>
+          <ul>
+            {todoStatus.todos.map((todo) => (
+              <TodoListItem key={todo.id} todo={todo} todoStatus={todoStatus} />
+            ))}
+          </ul>
+        </>
+      )}
+    </>
+  );
+};
+
+const useTodoStatus = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, title: "test1" },
+    { id: 2, title: "test2" },
+    { id: 3, title: "test3" },
+  ]);
+  const [lastTodoId, setLastTodoId] = useState(3);
+
+  const addTodo = (title) => {
+    const id = lastTodoId + 1;
+
+    setTodos(
+      produce(todos, (draft) => {
+        draft.push({ id, title });
+      })
+    );
+
+    setLastTodoId(id);
+  };
+
+  const removeTodo = (id) => {
+    setTodos(
+      produce(todos, (draft) => {
+        const index = draft.findIndex((todo) => todo.id === id);
+        draft.splice(index, 1);
+      })
+    );
+  };
+
+  const modifyTodo = (id, title) => {
+    setTodos(
+      produce(todos, (draft) => {
+        const index = draft.findIndex((todo) => todo.id === id);
+        draft[index].title = title;
+      })
+    );
+  };
+
+  return { todos, addTodo, removeTodo, modifyTodo };
+};
+
+export default function Home() {
+  const todoStatus = useTodoStatus();
+
+  return (
+    <>
+      <NewTodoForm todoStatus={todoStatus} />
+      <hr />
+      <TodoList todoStatus={todoStatus} />
+    </>
   );
 }
