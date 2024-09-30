4af848672b7853d254962856a278c2e9740fe5a8
Eichi2514 <picon1317@gmail.com>
2024-09-30 14:21:15 +0900

    MUI Drawer 사용

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,36 @@
 'use client';
 
 import * as React from 'react';
-import { AppBar, Toolbar, Button, Alert as MuiAlert, Snackbar } from '@mui/material';
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Alert as MuiAlert,
+  Snackbar,
+  Backdrop,
+  CircularProgress,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import { FaBars } from 'react-icons/fa';
 import theme from './theme';
 
-const Alert = React.forwardRef((props, ref) => {
-  return <MuiAlert {...props} ref={ref} />;
-});
-
 export default function App() {
   const [open, setOpen] = React.useState(false);
+  const [list, setList] = React.useState(['Cccccc', 'Rrrrrr', 'Uuuuuu', 'Dddddd']);
 
-  const alertRef = React.useRef(null);
+  const DrawerList = () => (
+    <List>
+      {list.map((text, index) => (
+        <ListItem button key={index}>
+          <ListItemText primary={text} />
+        </ListItem>
+      ))}
+    </List>
+  );
 
   return (
     <>
@@ -22,7 +39,7 @@ export default function App() {
         <AppBar position="fixed">
           <Toolbar>
             <div className="tw-flex-1">
-              <FaBars />
+              <FaBars className="tw-cursor-pointer" onClick={() => setOpen(true)} />
             </div>
             <div className="logo-box">
               <a href="/" className="tw-font-bold">
@@ -38,23 +55,9 @@ export default function App() {
       </ThemeProvider>
 
       <section>
-        <Button onClick={() => setOpen(true)}>Open Snackbar</Button>
-        <Alert ref={alertRef} severity="error" variant="filled">
-          게시물이 삭제됨
-        </Alert>
-        <Alert ref={alertRef} severity="success" variant="outlined">
-          성공 메세지
-        </Alert>
-        <Snackbar
-          open={open}
-          autoHideDuration={2000}
-          onClose={() => setOpen(false)}
-          message="Note archived">
-          <Alert variant="outlined" severity="error">
-            게시물 삭제{' '}
-            {/* 해당 Alert의 유무에 따라서 스낵바를 클릭했을 때 나오는 메세지가 달라짐*/}
-          </Alert>
-        </Snackbar>
+        <Drawer anchor="left" open={open} onClose={() => setOpen(false)}>
+          <DrawerList />
+        </Drawer>
       </section>
     </>
   );
